3a8245fd1f1f61d2163cdd72b905c383f3b1f638
Ivanov A <ivanov@web-premier.ru>
2020-02-14 18:38:48 +0500

    Отключение обязательного слова дял поиска

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -23,73 +23,71 @@ class SearchController extends Controller
 
         $results = [];
 
-        if(!$search OR mb_strlen($search) >= 3){
+        $finder = $this->container->get('fos_elastica.finder.app.catalog');
+        $boolQuery = new \Elastica\Query\BoolQuery();
 
-            $finder = $this->container->get('fos_elastica.finder.app.catalog');
-            $boolQuery = new \Elastica\Query\BoolQuery();
+        $fieldQuery = new \Elastica\Query\Term();
+        $fieldQuery->setTerm('moderated', true);
+        $boolQuery->addMust($fieldQuery);
 
-            $fieldQuery = new \Elastica\Query\Term();
-            $fieldQuery->setTerm('moderated', true);
-            $boolQuery->addMust($fieldQuery);
+        $fieldQuery = new \Elastica\Query\Term();
+        $fieldQuery->setTerm('block', false);
+        $boolQuery->addMust($fieldQuery);
 
-            $fieldQuery = new \Elastica\Query\Term();
-            $fieldQuery->setTerm('block', false);
-            $boolQuery->addMust($fieldQuery);
+        $fieldQuery = new \Elastica\Query\Term();
+        $fieldQuery->setTerm('deleted', false);
+        $boolQuery->addMust($fieldQuery);
 
-            $fieldQuery = new \Elastica\Query\Term();
-            $fieldQuery->setTerm('deleted', false);
-            $boolQuery->addMust($fieldQuery);
-
+        if($search OR mb_strlen($search) > 0) {
             $fieldQuery = new \Elastica\Query\Match();
             $fieldQuery->setFieldQuery('name', $search);
             //$fieldQuery->setFieldParam('name', 'type', 'phrase_prefix');
             $boolQuery->addMust($fieldQuery);
-
-            $software = $request->get('software');
-
-            if(is_array($software) AND count($software)>0){
-                $softwareNames = [];
-                foreach ($software as $eId){
-                    $eId = intval($eId);
-                    $softwareNames[] = $eId;
-                }
-
-                $fieldQuery = new \Elastica\Query\Terms();
-                $fieldQuery->setTerms('software', $softwareNames);
-                $boolQuery->addMust($fieldQuery);
-            }
-
-            $style = $request->get('style');
-
-            if(is_array($style) AND count($style)>0){
-                $styleNames = [];
-                foreach ($style as $eId){
-                    $eId = intval($eId);
-                    $styleNames[] = $eId;
-                }
-
-                $fieldQuery = new \Elastica\Query\Terms();
-                $fieldQuery->setTerms('style', $styleNames);
-                $boolQuery->addMust($fieldQuery);
-            }
-
-            $sort = $request->get('sort', 'date');
-
-            $finalQuery = new \Elastica\Query($boolQuery);
-
-            if($sort == 'date'){
-                $finalQuery->setSort(array('date' => array('order' => 'DESC')));
-            }elseif($sort == 'comments'){
-                $finalQuery->setSort(array('comments' => array('order' => 'DESC')));
-            }elseif($sort == 'views'){
-                $finalQuery->setSort(array('views' => array('order' => 'DESC')));
-            }else{
-                $finalQuery->setSort(array('date' => array('order' => 'DESC')));
-            }
-
-            $results = $finder->find($finalQuery);
-
         }
+
+        $software = $request->get('software');
+
+        if(is_array($software) AND count($software)>0){
+            $softwareNames = [];
+            foreach ($software as $eId){
+                $eId = intval($eId);
+                $softwareNames[] = $eId;
+            }
+
+            $fieldQuery = new \Elastica\Query\Terms();
+            $fieldQuery->setTerms('software', $softwareNames);
+            $boolQuery->addMust($fieldQuery);
+        }
+
+        $style = $request->get('style');
+
+        if(is_array($style) AND count($style)>0){
+            $styleNames = [];
+            foreach ($style as $eId){
+                $eId = intval($eId);
+                $styleNames[] = $eId;
+            }
+
+            $fieldQuery = new \Elastica\Query\Terms();
+            $fieldQuery->setTerms('style', $styleNames);
+            $boolQuery->addMust($fieldQuery);
+        }
+
+        $sort = $request->get('sort', 'date');
+
+        $finalQuery = new \Elastica\Query($boolQuery);
+
+        if($sort == 'date'){
+            $finalQuery->setSort(array('date' => array('order' => 'DESC')));
+        }elseif($sort == 'comments'){
+            $finalQuery->setSort(array('comments' => array('order' => 'DESC')));
+        }elseif($sort == 'views'){
+            $finalQuery->setSort(array('views' => array('order' => 'DESC')));
+        }else{
+            $finalQuery->setSort(array('date' => array('order' => 'DESC')));
+        }
+
+        $results = $finder->find($finalQuery);
 
         return [
             "products"=>$results
